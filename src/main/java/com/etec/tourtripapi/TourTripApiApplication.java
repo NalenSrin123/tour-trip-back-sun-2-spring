a018f7577a92f738a4e177e97473bce6f3539776
nalensrin11 <nalensrin2023@gmail.com>
2026-07-31 14:23:31 +0700

    first commit

---
+++ src/main/java/com/etec/tourtripapi/TourTripApiApplication.java
@@ -0,0 +1,13 @@
+package com.etec.tourtripapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourTripApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourTripApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etec/tourtripapi/TourTripApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etec.tourtripapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourTripApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
